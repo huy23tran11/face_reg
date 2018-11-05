9e666024b19258ac43ff8a08aa593df65b6b8555
Huy Tran <huy23tran11@gmail.com>
2018-11-05 09:15:08 -0800

    building components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation/Navigation.js'
+import Logo from './components/Logo/Logo.js'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js'
+import Rank from './components/Rank/Rank.js'
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+const app = new Clarifai.App({
+ apiKey: '20c75e570394466aa003dd611cf73608'
+});
+const particles = {
+                particles: {
+                  line_linked: {
+                    shadow: {
+                      enable: true,
+                      color: "#3CA9D1",
+                      blur: 5
+                    }
+                  }
+                }
+              }
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state ={
+      input: '',
+    }
+  }
+  onInputChange =(event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit =() => {
+    console.log('click');
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+    function(response) {
+      console.log(response)
+    },
+    function(err) {
+      console.log('no fucking face')
+    }
+  );
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Particles
+          className ='particles'
+          params={particles}/>
+         <Navigation />
+         <Logo />
+         <Rank/>
+        <ImageLinkForm onInputChange ={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+        {/*<FaceRecognition />*/}
       </div>
     );
   }
---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './ImageLinkForm.css'
+
+const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
+	return (
+		<div>
+			<p className = 'f3'>
+				{'Link your image right here'}
+			</p>
+			<div className ='ma3 pa4 br5 shadow-5 center w-60 form' >
+				<input className = 'f4 pa2 w-70' 
+						type ='text'
+						onChange ={onButtonSubmit}/> 
+				<button className = 'grow link dib pa3 bg-light-red w-30 f3'
+						onClick ={onButtonSubmit}>Detect</button> 
+			</div>
+		</div>
+	)
+}
+
+export default ImageLinkForm;

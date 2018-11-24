f04259ccba3036707b0d6fa2469e2f54264fc801
Huy Tran <huy23tran11@gmail.com>
2018-11-23 18:14:08 -0800

    fixing crashed image

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -6,7 +6,7 @@ const FaceRecognition = ({imageUrl, box}) =>{
 		(!imageUrl)? <h1> Waiting... </h1>:
 		<div className ='center ma'>
 			<div className ='absolute mt2'>
-				<img id = 'inputimage' alt ='faces' src = {imageUrl} width ='500px' height ='auto'/>
+				<img id = 'inputimage' alt ='Carshed Image' src = {imageUrl} width ='500px' height ='auto'/>
 				<div className = 'bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
 			</div>
 		</div>
--- src/components/ImageCard/ImageCard.js
+++ src/components/ImageCard/ImageCard.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ImageCard = ({imageLink})=> {
 	return (
 		<div className= "tc bg-light-green dib br1 pa1 ma2 grow bw2 shadow-5">
-			<img alt = 'YourImage' width ='auto' height ='200px' src = {imageLink}/>
+			<img alt = 'Carshed Image' width ='auto' height ='200px' src = {imageLink}/>
 			<div>
 
 			</div>
